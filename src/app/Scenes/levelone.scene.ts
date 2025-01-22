94ff59fd2bad30a4d6ee2e734ff5b09940459e94
Ruthellon <58911878+Ruthellon@users.noreply.github.com>
2025-01-22 09:52:24 -0500

    changing level 1

--- src/app/Scenes/levelone.scene.ts
+++ src/app/Scenes/levelone.scene.ts
@@ -2,7 +2,7 @@
 import { Attacker } from "../GameObjects/attacker.gameobject";
 import { Block } from "../GameObjects/block.gameobject";
 import { Button } from "../GameObjects/Utilities/button.gameobject";
-import { Vector2, Vector3 } from "../Utility/classes.model";
+import { BlankSceneInfo, Vector2, Vector3 } from "../Utility/classes.model";
 import { Game } from "../Utility/game.model";
 import { DefenseBaseLevel, eDefenderTypes } from "./defensebase.scene";
 import { eLayerTypes } from "./scene.interface";
@@ -17,8 +17,9 @@ export class LevelOneScene extends DefenseBaseLevel {
   protected get NextLevelName(): string {
     return 'leveltwo';
   }
+  private startingHealth = 0;
   protected get PlayerStartingHealth(): number {
-    return 10;
+    return this.startingHealth;
   }
   private secondsBetweenMonsters: number = 1;
   protected override get SecondsBetweenMonsters(): number {
@@ -43,11 +44,29 @@ export class LevelOneScene extends DefenseBaseLevel {
   protected override get EndingCells(): Vector2[] {
     return this.endingCells;
   }
+  private defenderSize = 0;
   protected get DefenderSize(): number {
-    return 100;
+    return this.defenderSize;
   }
+  private gridCellSize: number = 0;
   protected get GridCellSize(): number {
-    return 100;
+    return this.gridCellSize;
+  }
+
+  constructor() {
+    super();
+    let sceneInfo: BlankSceneInfo = JSON.parse(LevelOneScene.levelJSON);
+
+    if (sceneInfo) {
+      this.gridCellSize = sceneInfo.GridSize;
+      this.defenderSize = sceneInfo.GridSize * sceneInfo.DefSizeMulti;
+      this.startingCells = sceneInfo.StartingCells;
+      this.endingCells = sceneInfo.EndingCells;
+      this.credits = sceneInfo.Credits;
+      this.startingHealth = sceneInfo.Health;
+      //this.SetRounds(sceneInfo.Rounds);
+      //this.SetObstacles(sceneInfo.ObstacleCells);
+    }
   }
 
   override Update(deltaTime: number): void {
@@ -67,7 +86,7 @@ export class LevelOneScene extends DefenseBaseLevel {
 
   override Load(): void {
     super.Load();
-    Game.AddCredits(30);
+    Game.AddCredits(this.credits);
   }
 
   protected CreateNewAttacker(attackerCount: number): Attacker {
@@ -151,4 +170,7 @@ export class LevelOneScene extends DefenseBaseLevel {
     Game.CONTEXT.textAlign = "center";
     Game.CONTEXT.fillText('You Won!', Game.CANVAS_WIDTH / 2, Game.CANVAS_HEIGHT / 2 + 75);
   }
+
+  private credits: number = 0;
+  protected static levelJSON = '{"SceneName":"Level 1","GridSize":60,"DefSizeMulti":2,"StartingCells":[{"X":1,"Y":10},{"X":1,"Y":9},{"X":1,"Y":8},{"X":1,"Y":7},{"X":1,"Y":6},{"X":1,"Y":5},{"X":1,"Y":4}],"EndingCells":[{"X":23,"Y":10},{"X":23,"Y":9},{"X":23,"Y":8},{"X":23,"Y":7},{"X":23,"Y":6},{"X":23,"Y":5},{"X":23,"Y":4}],"ObstacleCells":[],"Credits":50,"Health":15,"Rounds":[{"EnemyBatches":[{"EnemyCountStart":10,"TimeBetweenStart":1,"BatchDelayTime":0,"EnemySpeed":8,"EnemyHealth":18,"EnemyValue":1,"EnemySize":30,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":false,"ShieldValue":0,"TimeBetweenCurrent":0,"EnemyCountCurrent":0}]},{"EnemyBatches":[{"EnemyCountStart":10,"TimeBetweenStart":1,"BatchDelayTime":0,"EnemySpeed":8,"EnemyHealth":18,"EnemyValue":1,"EnemySize":30,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":false,"ShieldValue":0,"TimeBetweenCurrent":0,"EnemyCountCurrent":0},{"EnemyCountStart":5,"TimeBetweenStart":1.5,"BatchDelayTime":0,"EnemySpeed":8,"EnemyHealth":27,"EnemyValue":3,"EnemySize":60,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":false,"ShieldValue":0,"TimeBetweenCurrent":0,"EnemyCountCurrent":0}]},{"EnemyBatches":[{"EnemyCountStart":10,"TimeBetweenStart":1,"BatchDelayTime":0,"EnemySpeed":10,"EnemyHealth":18,"EnemyValue":1,"EnemySize":30,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":false,"ShieldValue":0,"TimeBetweenCurrent":0,"EnemyCountCurrent":0},{"EnemyCountStart":10,"TimeBetweenStart":1.5,"BatchDelayTime":0,"EnemySpeed":8,"EnemyHealth":30,"EnemyValue":3,"EnemySize":60,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":false,"ShieldValue":0,"TimeBetweenCurrent":0,"EnemyCountCurrent":0},{"EnemyCountStart":5,"TimeBetweenStart":1.5,"BatchDelayTime":15,"EnemySpeed":10,"EnemyHealth":10,"EnemyValue":1,"EnemySize":30,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":false,"ShieldValue":10,"TimeBetweenCurrent":0,"EnemyCountCurrent":0}]},{"EnemyBatches":[{"EnemyCountStart":10,"TimeBetweenStart":1,"BatchDelayTime":0,"EnemySpeed":12,"EnemyHealth":21,"EnemyValue":1,"EnemySize":30,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":false,"ShieldValue":0,"TimeBetweenCurrent":0,"EnemyCountCurrent":0},{"EnemyCountStart":10,"TimeBetweenStart":1.5,"BatchDelayTime":0,"EnemySpeed":10,"EnemyHealth":30,"EnemyValue":3,"EnemySize":60,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":false,"ShieldValue":0,"TimeBetweenCurrent":0,"EnemyCountCurrent":0},{"EnemyCountStart":5,"TimeBetweenStart":1,"BatchDelayTime":15,"EnemySpeed":12,"EnemyHealth":15,"EnemyValue":1,"EnemySize":30,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":false,"ShieldValue":10,"TimeBetweenCurrent":0,"EnemyCountCurrent":0},{"EnemyCountStart":5,"TimeBetweenStart":1.5,"BatchDelayTime":0,"EnemySpeed":10,"EnemyHealth":20,"EnemyValue":3,"EnemySize":60,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":false,"ShieldValue":10,"TimeBetweenCurrent":0,"EnemyCountCurrent":0},{"EnemyCountStart":1,"TimeBetweenStart":0.5,"BatchDelayTime":20,"EnemySpeed":5,"EnemyHealth":300,"EnemyValue":15,"EnemySize":60,"EnemyDamage":5,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":false,"ShieldValue":0,"TimeBetweenCurrent":0,"EnemyCountCurrent":0}]},{"EnemyBatches":[{"EnemyCountStart":20,"TimeBetweenStart":1,"BatchDelayTime":0,"EnemySpeed":12,"EnemyHealth":20,"EnemyValue":1,"EnemySize":30,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":false,"ShieldValue":0,"TimeBetweenCurrent":0,"EnemyCountCurrent":0},{"EnemyCountStart":1,"TimeBetweenStart":1,"BatchDelayTime":25,"EnemySpeed":10,"EnemyHealth":60,"EnemyValue":3,"EnemySize":60,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":true,"ShieldValue":0,"TimeBetweenCurrent":0,"EnemyCountCurrent":0}]},{"EnemyBatches":[{"EnemyCountStart":15,"TimeBetweenStart":1,"BatchDelayTime":0,"EnemySpeed":12,"EnemyHealth":20,"EnemyValue":1,"EnemySize":30,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":false,"ShieldValue":0,"TimeBetweenCurrent":0,"EnemyCountCurrent":0},{"EnemyCountStart":15,"TimeBetweenStart":1,"BatchDelayTime":0,"EnemySpeed":10,"EnemyHealth":15,"EnemyValue":1,"EnemySize":30,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":false,"ShieldValue":10,"TimeBetweenCurrent":0,"EnemyCountCurrent":0},{"EnemyCountStart":10,"TimeBetweenStart":1.5,"BatchDelayTime":0,"EnemySpeed":10,"EnemyHealth":30,"EnemyValue":3,"EnemySize":60,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":false,"ShieldValue":0,"TimeBetweenCurrent":0,"EnemyCountCurrent":0},{"EnemyCountStart":10,"TimeBetweenStart":1.5,"BatchDelayTime":0,"EnemySpeed":10,"EnemyHealth":20,"EnemyValue":3,"EnemySize":60,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":false,"ShieldValue":15,"TimeBetweenCurrent":0,"EnemyCountCurrent":0},{"EnemyCountStart":5,"TimeBetweenStart":1,"BatchDelayTime":25,"EnemySpeed":10,"EnemyHealth":60,"EnemyValue":3,"EnemySize":60,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":true,"ShieldValue":0,"TimeBetweenCurrent":0,"EnemyCountCurrent":0}]},{"EnemyBatches":[{"EnemyCountStart":15,"TimeBetweenStart":1,"BatchDelayTime":0,"EnemySpeed":14,"EnemyHealth":25,"EnemyValue":1,"EnemySize":30,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":false,"ShieldValue":0,"TimeBetweenCurrent":0,"EnemyCountCurrent":0},{"EnemyCountStart":15,"TimeBetweenStart":1,"BatchDelayTime":0,"EnemySpeed":12,"EnemyHealth":15,"EnemyValue":1,"EnemySize":30,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":false,"ShieldValue":15,"TimeBetweenCurrent":0,"EnemyCountCurrent":0},{"EnemyCountStart":10,"TimeBetweenStart":1.25,"BatchDelayTime":0,"EnemySpeed":10,"EnemyHealth":40,"EnemyValue":3,"EnemySize":60,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":false,"ShieldValue":0,"TimeBetweenCurrent":0,"EnemyCountCurrent":0},{"EnemyCountStart":10,"TimeBetweenStart":1.25,"BatchDelayTime":0,"EnemySpeed":10,"EnemyHealth":25,"EnemyValue":3,"EnemySize":60,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":false,"ShieldValue":60,"TimeBetweenCurrent":0,"EnemyCountCurrent":0},{"EnemyCountStart":5,"TimeBetweenStart":2,"BatchDelayTime":10,"EnemySpeed":10,"EnemyHealth":75,"EnemyValue":5,"EnemySize":60,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":true,"ShieldValue":0,"TimeBetweenCurrent":0,"EnemyCountCurrent":0},{"EnemyCountStart":5,"TimeBetweenStart":2,"BatchDelayTime":15,"EnemySpeed":10,"EnemyHealth":50,"EnemyValue":5,"EnemySize":60,"EnemyDamage":1,"EnemyStartCells":[0,1,2,3,4,5,6],"EnemyCanFly":true,"ShieldValue":50,"TimeBetweenCurrent":0,"EnemyCountCurrent":0}]},{"EnemyBatches":[{"EnemyCountStart":1,"TimeBetweenStart":0.5,"BatchDelayTime":0,"EnemySpeed":8,"EnemyHealth":600,"EnemyValue":50,"EnemySize":60,"EnemyDamage":5,"EnemyStartCells":[3],"EnemyCanFly":false,"ShieldValue":300,"TimeBetweenCurrent":0,"EnemyCountCurrent":0}]}]}';
 }
